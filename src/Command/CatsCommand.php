23e9be3ec09f472b848e7d50f4bdf42d14e08602
Guillaume <gsylvestre@gmail.com>
2020-09-07 15:29:59 +0200

    create command to get random cats

---
+++ src/Command/CatsCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Cat;
+use Doctrine\ORM\EntityManagerInterface;
+use Faker\Factory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CatsCommand extends Command
+{
+    protected static $defaultName = 'app:cats';
+
+    private $client;
+    private $em;
+
+    //injection de mes dépendances
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->client = $client;
+        $this->em = $em;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('generates cats in db, download images')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        //utile pour écrire dans la console
+        $io = new SymfonyStyle($input, $output);
+
+        //photos de chatons random
+        $url = "https://api.unsplash.com/photos/random?query=kitten&client_id=f4b599bb478f23fe8c8fb588020abce521b3b7d93388627bd2d98980ac07e0ba&orientation=landscape&count=30&featured=true";
+        //HTTP Client pour faire la requête
+        $response = $this->client->request('GET', $url);
+        //convertie le json en array
+        $content = $response->toArray();
+
+        //pour générer des données bidons (en français)
+        $faker = Factory::create('fr_FR');
+
+        foreach($content as $pic) {
+            //crée des chatons
+            $cat = new Cat();
+            $name = $faker->unique()->firstName();
+            $cat->setName($name);
+            //juste pour avoir des noms d'image sans espace ni accent, uniques
+            $filename = sha1($pic['urls']['regular']) . ".jpeg";
+            $cat->setFilename($filename);
+            //download le fichier image
+            file_put_contents('public/img/' . $filename, file_get_contents($pic['urls']['regular']));
+
+            $cat->setDescription($faker->paragraphs($faker->randomDigitNotNull(), true));
+            $cat->setPrice($faker->numberBetween(10, 100));
+            $cat->setDateCreated($faker->dateTimeBetween("- 1 year"));
+
+            $this->em->persist($cat);
+            $io->text($cat->getName());
+        }
+
+        $this->em->flush();
+
+        $io->success('Done!');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Entity/Cat.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CatRepository::class)
+ */
+class Cat
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $filename;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCreated;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isSold;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getIsSold(): ?bool
+    {
+        return $this->isSold;
+    }
+
+    public function setIsSold(bool $isSold): self
+    {
+        $this->isSold = $isSold;
+
+        return $this;
+    }
+}
